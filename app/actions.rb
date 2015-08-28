796c2df842992f05915a67c3a1e10cc62b1331b5
Frank Lee <lee.frankie@gmail.com>
2015-08-27 20:49:51 -0400

    Rebuild homepage and signup
    Reworked all pages, working sign in, sign up and log off.

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,14 @@
-# Homepage (Root path)
+helpers do
+	def current_user
+	  @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+	end
+end
+
+# GET
+
+get '/index' do
+	erb :index
+end 
 
 get '/login' do
 	erb :login
@@ -7,3 +17,74 @@ end
 get '/profile' do
 	erb :profile
 end
+
+get '/signup' do
+	erb :signup
+end
+
+get '/logout' do
+	session[:user_id] = ""
+	redirect "/index"
+end
+
+get '/movies' do
+	erb :movies
+end
+
+post '/movies' do
+	title = params[:title] 
+	diretor = params[:director]
+	image_url = params[:image_url]
+
+	Movie.create({
+		title: title,
+		director: director,
+		image_url: image_url
+	})
+
+	redirect("/")
+	# movie = params (hash datatype)
+
+end
+
+get '/movies/:id' do
+	@movie = Movie.find(params[:id])
+	erb :show_movie
+end
+
+# POST
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user.password == password
+    session[:user_id] = user.id
+    redirect "/index"
+  else
+    redirect "/login"
+  end
+end
+
+post '/signup' do
+  username = params[:email]
+  email = params[:email]
+  password = params[:password]
+
+  user = User.find_by(email: email)
+  if user
+    redirect '/index'
+  else
+    user = User.create(username: username, password: password, email:email)
+    session[:user_id] = user.id
+    redirect '/signup'
+  end
+end
+
+post '/profile' do
+    redirect '/'
+end
+
+#LOGOUT
+
+
